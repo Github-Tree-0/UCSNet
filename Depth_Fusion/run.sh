95a0acb171ccbd5d3c84b6aadc22c9d4ee5c2b82
Github-Tree-0 <1900012967@pku.edu.cn>
2022-07-13 09:31:50 +0000

    generate point cloud

--- Depth_Fusion/run.sh
+++ Depth_Fusion/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-root_path="/cephfs/shuocheng/tanks_results"
+root_path="../small_dtu_results"
+save_path="../small_dtu_points"
+test_list="./dataloader/datalist/small_dtu/test.txt"
 
-python fuse.py --root_path $root_path --save_path "./points" \
-                --dist_thresh 0.001 --prob_thresh 0.6 --num_consist 10 --device "cuda"
+python ./Depth_Fusion/fuse.py --root_path $root_path --save_path $save_path \
+                --data_list $test_list --dist_thresh 0.001 --prob_thresh 0.6 \
+                --num_consist 10 --device "cuda"
