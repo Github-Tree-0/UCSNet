3197e7192d09d2bfbb65a0e78b1d3224aada02ba
Github-Tree-0 <1900012967@pku.edu.cn>
2022-07-28 05:48:17 +0000

    debug

--- test_point_cloud.py
+++ test_point_cloud.py
@@ -8,13 +8,62 @@ import torch.backends.cudnn as cudnn
 from dataloader.mvs_dataset import MVSTestSet
 from networks.ucsnet import UCSNet
 from utils.utils import dict2cuda, dict2numpy, mkdir_p, save_cameras, write_pfm
-from Depth_Fusion.fuse import extract_points, filter_depth
+from Depth_Fusion.warp_func import *
 
 import argparse, time, cv2
 import numpy as np
 from collections import *
 
 cudnn.benchmark = True
+
+
+
+def filter_depth(ref_depth, src_depths, ref_proj, src_projs):
+	'''
+	
+	:param ref_depth: (1, 1, H, W)
+	:param src_depths: (B, 1, H, W)
+	:param ref_proj: (1, 4, 4)
+	:param src_proj: (B, 4, 4)
+	:return: ref_pc: (1, 3, H, W), aligned_pcs: (B, 3, H, W), dist: (B, 1, H, W)
+	'''
+
+	ref_pc = generate_points_from_depth(ref_depth, ref_proj)
+	src_pcs = generate_points_from_depth(src_depths, src_projs)
+
+	aligned_pcs = homo_warping(src_pcs, src_projs, ref_proj, ref_depth)
+
+	# _, axs = plt.subplots(3, 4)
+	# for i in range(3):
+	# 	axs[i, 0].imshow(src_pcs[0, i], vmin=0, vmax=1)
+	# 	axs[i, 1].imshow(aligned_pcs[0, i],  vmin=0, vmax=1)
+	# 	axs[i, 2].imshow(ref_pc[0, i],  vmin=0, vmax=1)
+	# 	axs[i, 3].imshow(ref_pc[0, i] - aligned_pcs[0, i], vmin=-0.5, vmax=0.5, cmap='coolwarm')
+	# plt.show()
+
+	x_2 = (ref_pc[:, 0] - aligned_pcs[:, 0])**2
+	y_2 = (ref_pc[:, 1] - aligned_pcs[:, 1])**2
+	z_2 = (ref_pc[:, 2] - aligned_pcs[:, 2])**2
+	dist = torch.sqrt(x_2 + y_2 + z_2).unsqueeze(1)
+
+	return ref_pc, aligned_pcs, dist
+	
+def extract_points(pc, mask, rgb, prob, feature):
+	pc = pc.cpu().numpy()
+	mask = mask.cpu().numpy()
+
+	mask = np.reshape(mask, (-1,))
+	pc = np.reshape(pc, (-1, 3))
+	rgb = np.reshape(rgb, (-1, 3))
+	points_prob = np.reshape(prob, (-1,))[np.where(mask)]
+	points_feature = np.reshape(feature, (-1, feature.shape[-1]))[np.where(mask)]
+
+	points = pc[np.where(mask)]
+	colors = rgb[np.where(mask)]
+
+	points_with_color = np.concatenate([points, colors], axis=1)
+
+	return points_with_color, points_prob, points_feature
 
 def process_data(args, scene_results, thresh):
     depths = scene_results['depth']
@@ -192,7 +241,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_consist', type=int, default=10)
     parser.add_argument('--device', type=str, default='cpu')
 
-    args = parser.parse_args()
+    args = parser.parse_args([])
 
     with torch.no_grad():
         results = test_main(args)
